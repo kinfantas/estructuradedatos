9abbfd27a109d452ad0b4b8282e7c8e46991ed8d
Karem Infantas <kareminfantas@gmail.com>
2018-03-05 21:06:40 -0400

    Create triangulo.cpp

---
+++ triangulo.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "triangulo.h"
+//Implementacion de los metodos de la clase
+//Declaracion del constructor
+triangulo::triangulo(void)
+{
+	base=4;
+	altura=3;
+}
+//Declaracion del destructor
+triangulo::~triangulo(void)
+{
+}
+//Declaración del metodo - funcion
+float triangulo::calculararea(float b, float h)
+{
+	area=(b*h)/2;
+	return area;
+}
